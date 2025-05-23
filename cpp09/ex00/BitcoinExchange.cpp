83edd113568d780d427e92d90ff4cb490898581c
cantasar <62062865+cantasar@users.noreply.github.com>
2025-05-23 10:50:53 +0300

    STL

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -24,11 +24,21 @@ bool BitcoinExchange::isValidDate(const std::string& date) {
         int month = atoi(date.substr(5, 2).c_str());
         int day = atoi(date.substr(8, 2).c_str());
 
-        if (year < 2009 || year > 2024)
+        if (year < 2009)
             return false;
         if (month < 1 || month > 12)
             return false;
         if (day < 1 || day > 31)
+            return false;
+
+        if (month == 2) {
+            bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            if (isLeapYear && day > 29)
+                return false;
+            if (!isLeapYear && day > 28)
+                return false;
+        }
+        else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
             return false;
 
         return true;
@@ -93,7 +103,7 @@ void BitcoinExchange::process_input(const std::string& filename) {
 
             std::map<std::string, float>::iterator it = _database.lower_bound(date);
             if (it == _database.begin() && it->first != date) {
-                std::cout << "Error: no bitcoin price data available for this date." << std::endl;
+                std::cout << "Error: bad input => " << date << std::endl;
                 continue;
             }
             if (it == _database.end() || it->first != date)
